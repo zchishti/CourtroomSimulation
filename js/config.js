31ed651068fae2d7dcf11756ee97c8c74f5a2066
zchishti <zchishti91@gmail.com>
2021-08-24 14:07:41 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const APP_KEY = '772C0B7393B2D72DA1A37109666C49E1';
+const APP_KEY = 'API_KEY';
 const APP = 'CourtRoomSim';
 const COURSE_KEY = 'CourtroomTestCourseKey';
 const TERM_CODE = 'CourtroomTestTermCodes';
